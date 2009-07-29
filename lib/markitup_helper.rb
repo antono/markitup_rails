3bcfc0b137f2450c049f63b271ece9c2016fca03
Antono Vasiljev <antono.vasiljev@gmail.com>
2009-07-29 18:11:37 +0300

    output all assets tags

--- lib/markitup_helper.rb
+++ lib/markitup_helper.rb
@@ -3,12 +3,12 @@ module MarkitupHelper
   def include_markitup_assets(options = {})
     options[:sets] ||= 'default'
     options[:skin] ||= 'markitup'
-
-    javascript_include_tag File.join(MARKITUP_ASSETS_BASE, 'jquery.markitup.pack.js') 
-    javascript_include_tag File.join(MARKITUP_ASSETS_BASE, 'sets', options[:sets].to_s, 'set.js')
-
-    stylesheet_link_tag File.join(MARKITUP_ASSETS_BASE, 'skins', options[:skin].to_s, 'style.css')
-    stylesheet_link_tag File.join(MARKITUP_ASSETS_BASE, 'sets', options[:sets].to_s, 'style.css')
+    html = []
+    html << javascript_include_tag File.join(MARKITUP_ASSETS_BASE, 'jquery.markitup.pack.js') 
+    html << javascript_include_tag File.join(MARKITUP_ASSETS_BASE, 'sets', options[:sets].to_s, 'set.js')
+    html << stylesheet_link_tag File.join(MARKITUP_ASSETS_BASE, 'skins', options[:skin].to_s, 'style.css')
+    html << stylesheet_link_tag File.join(MARKITUP_ASSETS_BASE, 'sets', options[:sets].to_s, 'style.css')
+    html.join("\n")
   end
 
   def init_markitup_for(css3_selector)
