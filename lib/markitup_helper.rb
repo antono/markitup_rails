8acd1933018e17c968c01e59477270f5146a762c
Antono Vasiljev <antono.vasiljev@gmail.com>
2009-07-29 18:15:43 +0300

    fix: ml << stylesheet_link_tag File.join(MARKITUP_ASSETS_BASE,

--- lib/markitup_helper.rb
+++ lib/markitup_helper.rb
@@ -4,10 +4,10 @@ module MarkitupHelper
     options[:sets] ||= 'default'
     options[:skin] ||= 'markitup'
     html = []
-    html << javascript_include_tag File.join(MARKITUP_ASSETS_BASE, 'jquery.markitup.pack.js') 
-    html << javascript_include_tag File.join(MARKITUP_ASSETS_BASE, 'sets', options[:sets].to_s, 'set.js')
-    html << stylesheet_link_tag File.join(MARKITUP_ASSETS_BASE, 'skins', options[:skin].to_s, 'style.css')
-    html << stylesheet_link_tag File.join(MARKITUP_ASSETS_BASE, 'sets', options[:sets].to_s, 'style.css')
+    html << javascript_include_tag(File.join(MARKITUP_ASSETS_BASE, 'jquery.markitup.pack.js'))
+    html << javascript_include_tag(File.join(MARKITUP_ASSETS_BASE, 'sets', options[:sets].to_s, 'set.js'))
+    html << stylesheet_link_tag(File.join(MARKITUP_ASSETS_BASE, 'skins', options[:skin].to_s, 'style.css'))
+    html << stylesheet_link_tag(File.join(MARKITUP_ASSETS_BASE, 'sets', options[:sets].to_s, 'style.css'))
     html.join("\n")
   end
 
