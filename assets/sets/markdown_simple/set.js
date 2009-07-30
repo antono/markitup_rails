490c0a1e72977cb92b6abe6a68299ca18f025728
Antono Vasiljev <antono.vasiljev@gmail.com>
2009-07-30 05:07:13 +0300

    back order of keys

--- assets/sets/markdown_simple/set.js
+++ assets/sets/markdown_simple/set.js
@@ -15,9 +15,6 @@ mySettings = {
 	previewParserPath:	'',
 	onShiftEnter:		{keepDefault:false, openWith:'\n\n'},
 	markupSet: [
-		{name:'Bold', key:'B', openWith:'**', closeWith:'**'},
-		{name:'Italic', key:'I', openWith:'_', closeWith:'_'},
-		{separator:'---------------' },
 		{name:'First Level Heading', key:'1', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '=') } },
 		{name:'Second Level Heading', key:'2', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '-') } },
 		{name:'Heading 3', key:'3', openWith:'### ', placeHolder:'Your title here...' },
@@ -25,6 +22,9 @@ mySettings = {
 		{name:'Heading 5', key:'5', openWith:'##### ', placeHolder:'Your title here...' },
 		{name:'Heading 6', key:'6', openWith:'###### ', placeHolder:'Your title here...' },
 		{separator:'---------------' },		
+		{name:'Bold', key:'B', openWith:'**', closeWith:'**'},
+		{name:'Italic', key:'I', openWith:'_', closeWith:'_'},
+		{separator:'---------------' },
 		{name:'Bulleted List', openWith:'- ' },
 		{name:'Numeric List', openWith:function(markItUp) {
 			return markItUp.line+'. ';
