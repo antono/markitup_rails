e3ba1eefe685ba21812716fcdd3cd480e7565e99
Antono Vasiljev <antono.vasiljev@gmail.com>
2009-07-30 04:45:54 +0300

    Use  FileUtils.cp_r 'src/.', 'dest'

--- install.rb
+++ install.rb
@@ -1,5 +1,5 @@
 puts "\n===> Copying files..."
-src = File.join(File.dirname(__FILE__), 'assets/*')
+src = File.join(File.dirname(__FILE__), 'assets/.')
 dst = File.join(RAILS_ROOT, "public/plugin_assets/markitup/")
 FileUtils.mkdir_p dst
 FileUtils.cp_r(src, dst, :verbose => true)
